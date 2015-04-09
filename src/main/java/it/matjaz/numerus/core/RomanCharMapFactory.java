856e65cea4e5c29ff4957672bfa460f0bfc8d399
Matjaž <dev@matjaz.it>
2015-04-09 12:35:41 +0200

    Add charMap generation and test is completely
    The charMap contains only the correct pairs <romanChar, itsIntValue>

--- src/main/java/it/matjaz/numerus/core/RomanCharMapFactory.java
+++ src/main/java/it/matjaz/numerus/core/RomanCharMapFactory.java
@@ -1,13 +1,26 @@
 package it.matjaz.numerus.core;
 
 // @author Matjaž <a href="mailto:dev@matjaz.it">dev@matjaz.it</a> <a href="http://matjaz.it">www.matjaz.it</a>
-
 import java.util.HashMap;
 import java.util.Map;
 
-
 public class RomanCharMapFactory {
-    public static Map generateCharMap() {
-        return new HashMap();
+
+    public static Map<String, Integer> generateCharMap() {
+        Map<String, Integer> charMap = new HashMap();
+        charMap.put("M", 1000);
+        charMap.put("CM", 900);
+        charMap.put("D", 500);
+        charMap.put("CD", 400);
+        charMap.put("C", 100);
+        charMap.put("XC", 90);
+        charMap.put("L", 50);
+        charMap.put("XL", 40);
+        charMap.put("X", 10);
+        charMap.put("IX", 9);
+        charMap.put("V", 5);
+        charMap.put("IV", 4);
+        charMap.put("I", 1);
+        return charMap;
     }
 }
--- src/test/java/it/matjaz/numerus/core/RomanCharMapFactoryTest.java
+++ src/test/java/it/matjaz/numerus/core/RomanCharMapFactoryTest.java
@@ -12,9 +12,9 @@ import org.junit.Test;
  * <a href="http://matjaz.it">www.matjaz.it</a>
  */
 public class RomanCharMapFactoryTest {
-    
-    private Map charMap;
-    
+
+    private Map<String, Integer> charMap;
+
     @Before
     public void generateCharMap() {
         this.charMap = RomanCharMapFactory.generateCharMap();
@@ -24,23 +24,65 @@ public class RomanCharMapFactoryTest {
     public void staticConstructorCreatesAMap() {
         Assert.assertThat(charMap, instanceOf(Map.class));
     }
-    
-//    @Test
-//    public void staticConstructorCreatesNonEmptyMap() {
-//        Assert.assertFalse(charMap.isEmpty());
-//    }
 
-//    @Test
-//    public void charMapKeysAreStrings() {
-//        Map charMap = RomanCharMapFactory
-//        Assert.assertThat(RomanCharMapFactory.generateCharMap().keySet()., instanceOf(Set.class));
-//    }
-//    @Test
-//    public void charMapContainsOnlyRomanChars() {
-//        Map<String, Integer> charMap = RomanCharMapFactory.generateCharMap();
-//        for (String romanChar : charMap.keySet()) {
-//            "[^MDCLXVI]";
-//        }
-//    }
+    @Test
+    public void staticConstructorCreatesNonEmptyMap() {
+        Assert.assertFalse(charMap.isEmpty());
+    }
 
+    @Test
+    public void charMapKeysAreStrings() {
+        Assert.assertThat(charMap.keySet().iterator().next(), instanceOf(String.class));
+    }
+
+    @Test
+    public void charMapValuesAreIntegers() {
+        Assert.assertThat(charMap.values().iterator().next(), instanceOf(Integer.class));
+    }
+
+    @Test
+    public void charMapKeysAreOnlyRomanChars() {
+        for (String romanChar : charMap.keySet()) {
+            Assert.assertTrue(romanChar.matches("[MDCLXVI]+"));
+        }
+    }
+
+    @Test
+    public void charMapKeysAreAllTheRomanChars() {
+        Assert.assertTrue(charMap.containsKey("M"));
+        Assert.assertTrue(charMap.containsKey("CM"));
+        Assert.assertTrue(charMap.containsKey("D"));
+        Assert.assertTrue(charMap.containsKey("CD"));
+        Assert.assertTrue(charMap.containsKey("C"));
+        Assert.assertTrue(charMap.containsKey("XC"));
+        Assert.assertTrue(charMap.containsKey("L"));
+        Assert.assertTrue(charMap.containsKey("XL"));
+        Assert.assertTrue(charMap.containsKey("X"));
+        Assert.assertTrue(charMap.containsKey("IX"));
+        Assert.assertTrue(charMap.containsKey("V"));
+        Assert.assertTrue(charMap.containsKey("IV"));
+        Assert.assertTrue(charMap.containsKey("I"));
+    }
+
+    @Test
+    public void charMapContainsNoOtherKeys() {
+        Assert.assertTrue(charMap.size() == 13);
+    }
+
+    @Test
+    public void charMapValuesMatchRomanChars() {
+        Assert.assertTrue(charMap.get("M") == 1000);
+        Assert.assertTrue(charMap.get("CM") == 900);
+        Assert.assertTrue(charMap.get("D") == 500);
+        Assert.assertTrue(charMap.get("CD") == 400);
+        Assert.assertTrue(charMap.get("C") == 100);
+        Assert.assertTrue(charMap.get("XC") == 90);
+        Assert.assertTrue(charMap.get("L") == 50);
+        Assert.assertTrue(charMap.get("XL") == 40);
+        Assert.assertTrue(charMap.get("X") == 10);
+        Assert.assertTrue(charMap.get("IX") == 9);
+        Assert.assertTrue(charMap.get("V") == 5);
+        Assert.assertTrue(charMap.get("IV") == 4);
+        Assert.assertTrue(charMap.get("I") == 1);
+    }
 }
