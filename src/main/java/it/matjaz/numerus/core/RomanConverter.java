5f3783ef851b5f88afba1c4cddd93daa05cead2d
Matjaž <dev@matjaz.it>
2015-04-09 23:25:59 +0200

    Improve class JavaDoc and add integerToRomanString() JavaDoc
    Add paragraph tags to other romanStringToInteger() JavaDoc.

--- src/main/java/it/matjaz/numerus/core/RomanConverter.java
+++ src/main/java/it/matjaz/numerus/core/RomanConverter.java
@@ -3,11 +3,12 @@ package it.matjaz.numerus.core;
 import javafx.util.Pair;
 
 /**
- * Offers conversion methods from roman numerals to arabic integers and
- * vice-versa.
- *
- * Uses RomanCharMapFactory generated structures as reference for translations
- * of the digits in both numeral systems.
+ * Offers conversion methods from roman numerals as Strings to arabic numerals
+ * as Integers and vice-versa.
+ * <p>
+ * Uses a {@link RomanCharMapFactory} generated array of pairs (romanChar, its
+ * integer value) as reference for translations of the digits in both numeral
+ * systems.
  *
  * @author Matjaž <a href="mailto:dev@matjaz.it">dev@matjaz.it</a>
  * <a href="http://matjaz.it">www.matjaz.it</a>
@@ -27,11 +28,11 @@ public class RomanConverter {
     /**
      * Converts the given syntactically correct roman numeral as String to its
      * int value.
-     *
+     * <p>
      * This method performs no checks on the correctness of the roman numeral
      * String so the correct result is <b>not</b> guaranteed if the input is
      * incorrect.
-     *
+     * <p>
      * Works with 2 indexes: one is iterating through the given romanString, the
      * other through the reference array of Pairs
      * {@link RomanCharMapFactory#generateCharPairsArray() (roman char, value)}.
@@ -59,13 +60,27 @@ public class RomanConverter {
         }
         return arabicValue;
     }
-    
+
     /**
-     * 
-     * Based on            
-     * http://www.fredosaurus.com/notes-java/examples/components/romanNumerals/romanNumeral.html
-     * @param arabic
-     * @return 
+     * Converts the given int value to its representation in roman numerals as
+     * String.
+     * <p>
+     * Throws an exception if the given int is not positve or is bigger than
+     * 3999, which are the extremes of the roman numerals range of
+     * representation.
+     * <p>
+     * The algorithm is based on the
+     * <a href="http://www.fredosaurus.com/notes-java/examples/components/romanNumerals/romanNumeral.html">fredosaurus.com</a>
+     * roman numerals example. Iterates through the array of roman chars and
+     * appends as many chars as necessary to the output string so that the value
+     * could not be decreased anymore with the same roman char. Then switches to
+     * a char with less value.
+     *
+     * @param arabic int to be converted to a roman numeral as String.
+     * @return a string representing a sytactically correct roman numeral with
+     * the given value.
+     * @throws IllegalArgumentException if the arabic int is not in [1, 3999]
+     * range.
      */
     public String integerToRomanString(int arabic) {
         if (arabic < 1 || arabic > 3999) {
