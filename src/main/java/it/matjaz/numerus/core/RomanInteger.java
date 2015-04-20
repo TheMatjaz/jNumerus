41358259ac1b6758f04ba4623a33ab2640d465ed
Matjaž <dev@matjaz.it>
2015-04-20 23:29:37 +0200

    Add JavaDoc to RomanInteger
    Improve existing class JavaDoc, also with links. Add JavaDoc to all
    methods, clean variable name in equals(). Use same JavaDoc format for
    equals(), toString() and hashCode() as in RomanNumeral.

--- src/main/java/it/matjaz/numerus/core/RomanInteger.java
+++ src/main/java/it/matjaz/numerus/core/RomanInteger.java
@@ -14,35 +14,73 @@ package it.matjaz.numerus.core;
 import java.util.Objects;
 
 /**
- * Container for a RomanNumeral and its Integer value tied toghether.
+ * Container for a {@link RomanNumeral} and its Integer value tied toghether.
  * <p>
- * Upon initialization or modification of one of the two members, the other gets
- * immediatly updated. The roman numeral is always syntactically correct since
- * it's a RomanNumeral.
+ * This class keeps an {@link Integer} value and its representation in roman
+ * numerals as a {@link RomanNumeral} always correctly correlated with each
+ * other. Upon initialization or modification of one of the two members, the
+ * other gets immediatly updated. The roman numeral is always syntactically
+ * correct since it's a RomanNumeral.
  * <p>
- * Uses a private RomanConverter for the conversion of fields.
+ * Uses a private {@link RomanConverter} for the conversion of the fields.
  *
  * @author Matjaž <a href="mailto:dev@matjaz.it">dev@matjaz.it</a>
  * <a href="http://matjaz.it">matjaz.it</a>
  */
 public class RomanInteger {
 
+    /**
+     * The arabic value of the roman numeral.
+     */
     private int value;
+
+    /**
+     * The representation of the value in roman numerals.
+     */
     private RomanNumeral numeral;
+
+    /**
+     * The converter used to switch from roman numerals to integers.
+     */
     private final RomanConverter converter;
 
+    /**
+     * Creates an uninitialized RomanInteger.
+     * <p>
+     * The int values is set to zero and the roman numeral is costructed with
+     * the default {@link RomanNumeral} constructor.
+     */
     public RomanInteger() {
         this.converter = new RomanConverter();
         this.value = 0;
         this.numeral = new RomanNumeral();
     }
 
+    /**
+     * Creates a RomanInteger using the given int value.
+     * <p>
+     * The value gets immediatly converted to its equivalent
+     * {@link RomanNumeral}. If its out of range, and exception is thrown by the
+     * {@link RomanConverter} and the object is not constructed.
+     *
+     * @param value of the RomanInteger
+     */
     public RomanInteger(int value) {
         this.converter = new RomanConverter();
         this.numeral = converter.integerToRomanNumeral(value);
         this.value = value;
     }
 
+    /**
+     * Creates a RomanInteger using the given {@link RomanNumeral}.
+     * <p>
+     * The {@link RomanNumeral} gets immediatly converted to its equivalent int
+     * values. If the {@link RomanNumeral} is not initialized, an
+     * {@link IllegalArgumentException} is thrown and the object is not
+     * constructed.
+     *
+     * @param numeral in roman numerals of the RomanInteger
+     */
     public RomanInteger(RomanNumeral numeral) {
         if (!numeral.isInitialized()) {
             throw new IllegalArgumentException("Could not create a RomanInteger with uninitialized RomanNumeral.");
@@ -52,19 +90,48 @@ public class RomanInteger {
         this.numeral = numeral;
     }
 
+    /**
+     * Getter of the arabic int value.
+     *
+     * @return the int value of this RomanInteger.
+     */
     public int getValue() {
         return value;
     }
 
+    /**
+     * Getter of the {@link RomanNumeral}.
+     *
+     * @return the representation of this RomanInteger in roman numerals.
+     */
     public RomanNumeral getNumeral() {
         return numeral;
     }
 
+    /**
+     * Setter of this RomanInteger with the int value.
+     * <p>
+     * The value gets immediatly converted to its equivalent
+     * {@link RomanNumeral}. If its out of range, and exception is thrown by the
+     * {@link RomanConverter} and the object is not modified.
+     *
+     * @param value of this RomanInteger.
+     */
     public void setValue(int value) {
         this.numeral = converter.integerToRomanNumeral(value);
         this.value = value;
     }
 
+    /**
+     * Setter of this RomanInteger using the given {@link RomanNumeral}.
+     * <p>
+     * The {@link RomanNumeral} gets immediatly converted to its equivalent int
+     * values. If the {@link RomanNumeral} is not initialized, an
+     * {@link IllegalArgumentException} is thrown and the object is not
+     * modified.
+     *
+     * @param numeral in roman numerals of this RomanInteger.
+     */
     public void setNumeral(RomanNumeral numeral) {
         if (!numeral.isInitialized()) {
             throw new IllegalArgumentException("Could not set a RomanInteger with uninitialized RomanNumeral.");
@@ -73,6 +140,16 @@ public class RomanInteger {
         this.value = converter.romanNumeralToInteger(numeral);
     }
 
+    /**
+     * Returns the hash of this RomanInteger.
+     * <p>
+     * The hashcode is created using the int value and the RomanNumeral. Uses
+     * {@link Objects#hashCode(java.lang.Object)} and overrides
+     * {@link Object#hashCode()}.
+     *
+     * @return the hash of this RomanInteger.
+     * @see Object#hashCode()
+     */
     @Override
     public int hashCode() {
         int hash = 7;
@@ -81,21 +158,40 @@ public class RomanInteger {
         return hash;
     }
 
+    /**
+     * Verifies if the passed Object is equal to this.
+     * <p>
+     * Returns <code>true</code> if the passed Object is a RomanInteger and
+     * contains the same roman numerals String and Integer as this one, else
+     * <code>false</code>.
+     *
+     * @param otherRomanInteger to compare with this.
+     * @return a boolean telling if the two RomanIntegers are equal.
+     * @see Object#equals(java.lang.Object)
+     */
     @Override
-    public boolean equals(Object obj) {
-        if (obj == null) {
+    public boolean equals(Object otherRomanInteger) {
+        if (otherRomanInteger == null) {
             return false;
         }
-        if (getClass() != obj.getClass()) {
+        if (getClass() != otherRomanInteger.getClass()) {
             return false;
         }
-        final RomanInteger other = (RomanInteger) obj;
+        final RomanInteger other = (RomanInteger) otherRomanInteger;
         if (this.value != other.getValue()) {
             return false;
         }
         return this.numeral.equals(other.getNumeral());
     }
 
+    /**
+     * Returns a String representation of this RomanInteger, which is its
+     * Integer value followed by the roman numeral String.
+     *
+     * @return a String containing the value and roman numeral of this
+     * RomanInteger.
+     * @see Object#toString()
+     */
     @Override
     public String toString() {
         return value + " " + numeral.toString();
