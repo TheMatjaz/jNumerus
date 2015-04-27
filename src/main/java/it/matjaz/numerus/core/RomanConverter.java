9f8b3a04eb1f0673bf377546e3202a3981c980aa
Matjaž <dev@matjaz.it>
2015-04-28 01:18:59 +0200

    Add missing trailing slash to all matjaz.it/numerus/ links
    Also add in .svg logo file and POM configuration.

--- src/main/java/it/matjaz/numerus/core/RomanConverter.java
+++ src/main/java/it/matjaz/numerus/core/RomanConverter.java
@@ -3,7 +3,7 @@
  *
  * This Source Code Form is part of the project Numerus, a roman numerals
  * library for Java. The library and its source code may be found on:
- * https://github.com/MatjazDev/Numerus and http://matjaz.it/numerus
+ * https://github.com/MatjazDev/Numerus and http://matjaz.it/numerus/
  *
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
