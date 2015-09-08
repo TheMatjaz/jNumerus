679537c1dd0520710e2a9f1a33bd716b497904d9
Matjaž <dev@matjaz.it>
2015-09-08 15:32:08 +0200

    Add MAX and MIN values constant fields to RomanInteger

--- src/main/java/it/matjaz/numerus/RomanInteger.java
+++ src/main/java/it/matjaz/numerus/RomanInteger.java
@@ -52,6 +52,20 @@ public class RomanInteger extends Number implements Cloneable, Comparable<Number
     private static final long serialVersionUID = 20150428L;
 
     /**
+     * The biggest Integer convertable to RomanNumeral with the standard syntax.
+     */
+    public static final int MAXINTEGER = 3999;
+
+    /**
+     * The smallest Integer convertable to RomanNumeral with the standard
+     * syntax.
+     *
+     * See also {@link RomanNumeral#NULLA RomanNumeral.NULLA} for the String
+     * with the equivalent value.
+     */
+    public static final int MININTEGER = 0;
+
+    /**
      * The arabic value of the roman numeral.
      */
     private int value;
